f3106c82af6996e8bf2447877b7fae140407f7f2
Tilican <titben047@hotmail.fr>
2017-11-10 14:23:30 +0100

    unit tests

--- test/library.spec.js
+++ test/library.spec.js
@@ -1,23 +1,79 @@
 /* global describe, it, before */
 import chai from 'chai';
-// import tcp, { udp } from '../lib/library.min.js';
+import { udp, tcp } from '../lib/library.min.js';
 
 chai.expect();
 
 const expect = chai.expect;
 
-describe('TCP tests', () => {
-  describe('Null', () => {
-    it('should return ', () => {
-      expect(null).to.be.equal(null);
+let data = {
+  'Boolean': [
+    true,
+    false
+  ],
+  'Integer': [
+    42,
+    -42
+  ],
+  'Float': [
+    -42.4,
+    42.4
+  ],
+  'String': [
+    'test',
+    'test2'
+  ],
+  'Dictionary': [
+    {
+      true: false,
+      12: -12,
+      'test': 'test'
+    }
+  ],
+  'Array': [
+    [
+      true,
+      false,
+      12,
+      -12,
+      'test',
+      'test2'
+    ]
+  ]
+};
+
+describe('UDP test', () => {
+  for (let i in data) {
+    describe(i, () => {
+      for (let j = 0; j < data[i].length; j++) {
+        let value = data[i][j];
+
+        it(`encode(${value.toString()}) == encode(${value.toString()})`, () => {
+          expect(udp.encode(value)).to.deep.equal(udp.encode(value));
+        });
+
+        it(`${value.toString()} ==  decode(encode(${value.toString()}))`, () => {
+          expect(value).to.deep.equal(udp.decode(udp.encode(value)));
+        });
+      }
     });
-  });
+  }
 });
 
-describe('UDP tests', () => {
-  describe('Null', () => {
-    it('should return ', () => {
-      expect(null).to.be.equal(null);
+describe('TCP test', () => {
+  for (let i in data) {
+    describe(i, () => {
+      for (let j = 0; j < data[i].length; j++) {
+        let value = data[i][j];
+
+        it(`encode(${value.toString()}) == encode(${value.toString()})`, () => {
+          expect(tcp.encode(value)).to.deep.equal(tcp.encode(value));
+        });
+
+        it(`${value.toString()} ==  decode(encode(${value.toString()}))`, () => {
+          expect(value).to.deep.equal(tcp.decode(tcp.encode(value)));
+        });
+      }
     });
-  });
+  }
 });
